5a2c311b1b342577c1bff9f5bdcea5b12b6d27dc
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-05-22 01:15:25 +0200

    ajax : maths et jquery ecodroit, e4, e5, e6

---
+++ ajax/aj_moyenneMaths.php
@@ -0,0 +1,17 @@
+<?php
+if (!isset($_POST['maths'])) {
+    $_POST['maths'] = 0 ;
+}
+
+if (!isset($_POST['algo'])) {
+    $_POST['algo'] = 0 ;
+}
+
+$maths = $_POST['maths'] ;
+$algo = $_POST['algo'] ;
+
+$notemaths = $maths*2 ;
+$notealgo = $algo ;
+$moyennemaths = ($notemaths + $notealgo)/3 ;
+
+echo $moyennemaths ;
--- ajax/aj_recup.js
+++ ajax/aj_recup.js
@@ -14,3 +14,46 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $('input[name|="txt_maths"], input[name|="txt_algo"]').change(function () {
+        var maths = $('#txt_maths').val();
+        var algo = $('#txt_algo').val() ;
+        $.ajax({
+            url: "ajax/aj_moyenneMaths.php",
+            type: "POST",
+            data: {maths : maths, algo : algo},
+            success: function (data)
+            {
+                $("#partie_maths").val(data);
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('#txt_ecodroit').change(function () {
+        var value = $(this).val() ;
+        $("#partie_ecodroit").val(value);
+    });
+});
+
+$(document).ready(function () {
+    $('#txt_e4').change(function () {
+        var value = $(this).val() ;
+        $("#partie_e4").val(value);
+    });
+});
+
+$(document).ready(function () {
+    $('#txt_e5').change(function () {
+        var value = $(this).val() ;
+        $("#partie_e5").val(value);
+    });
+});
+
+$(document).ready(function () {
+    $('#txt_e6').change(function () {
+        var value = $(this).val() ;
+        $("#partie_e6").val(value);
+    });
+});
