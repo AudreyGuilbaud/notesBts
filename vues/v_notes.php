0ae6a3850e05a9169d3b13ad979a4cb28d16c2a1
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-05-21 19:23:11 +0200

    mise en place formulaires

--- vues/v_notes.php
+++ vues/v_notes.php
@@ -5,7 +5,7 @@
             <img class="stylo" src="./images/styloplume.gif">
         </div>
         <div class="divForm">
-            
+            <?php include_once("vues/v_notesGenerales.php") ; ?>
         </div>
     </div>
     <div class="divMatiere">
@@ -14,7 +14,7 @@
             <img class="crayon" src="./images/crayon.gif">
         </div>
         <div class="divForm">
-            
+            <?php include_once("vues/v_notesTechniques.php") ; ?>
         </div>
     </div>
     
---
+++ vues/v_notesTechniques.php
@@ -0,0 +1,36 @@
+<br><br>
+<div class="titrePartie">
+    <input name="partie_e4" id="partie_e4" class="inputNoteGen" type="text" readonly>
+    <label for="partie_e4">&nbsp;&nbsp;E4 : Conception et maintenance de solutions informatiques</label>
+
+</div>
+<br>
+<div class="partieNotes">
+    <input name="txt_e4" id="txt_e4" class="inputNote" type="text">
+    <label for="txt_e4">&nbsp;&nbsp;Conception et maintenance de solutions informatiques : </label>   
+</div>
+<br><br><br>
+<div class="titrePartie">
+    <input name="partie_e5" id="partie_e5" class="inputNoteGen" type="text" readonly>
+    <label for="partie_e5">&nbsp;&nbsp;E5 : Production et fourniture de services informatiques</label>
+
+</div>
+<br>
+<div class="partieNotes">
+    <input name="txt_e5" id="txt_e5" class="inputNote" type="text">
+    <label for="txt_e5">&nbsp;&nbsp;Production et fourniture de services informatiques : </label>   
+</div>
+<br><br><br>
+<div class="titrePartie">
+    <input name="partie_e6" id="partie_e6" class="inputNoteGen" type="text" readonly>
+    <label for="partie_e6">&nbsp;&nbsp;E6 : Parcours de professionnalisation</label>
+
+</div>
+<br>
+<div class="partieNotes">
+    <input name="txt_e6" id="txt_e6" class="inputNote" type="text">
+    <label for="txt_e6">&nbsp;&nbsp;Parcours de professionnalisation : </label>   
+</div>
+
+
+
