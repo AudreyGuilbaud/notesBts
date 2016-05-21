37262a32d74dd683837dc84b88a56e05ec5a1084
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-05-21 19:30:02 +0200

    finalisation entrée des notes

--- vues/v_notes.php
+++ vues/v_notes.php
@@ -1,7 +1,7 @@
 <div class="divGeneral">
     <div class="divMatiere">
         <div class="divTitre">
-            <span>Matières générales</span>
+            <span>Matières générales <input type="text" name="moyenne_gen" id="moyenne_gen" class="moyennes" readonly></span>
             <img class="stylo" src="./images/styloplume.gif">
         </div>
         <div class="divForm">
@@ -10,7 +10,7 @@
     </div>
     <div class="divMatiere">
         <div class="divTitre">
-            <span>Matières techniques</span>
+            <span>Matières techniques <input type="text" name="moyenne_tech" id="moyenne_tech" class="moyennes" readonly></span>
             <img class="crayon" src="./images/crayon.gif">
         </div>
         <div class="divForm">
