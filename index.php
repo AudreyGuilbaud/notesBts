4f1167e94432dfe1ac9966e15a9fa0d50f3c2ab3
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-05-22 02:12:07 +0200

    amélioration esthétique/ajout du pied

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ include_once("vues/v_entete.html") ;
 include_once("vues/v_panneau.html") ;
 include_once("vues/v_resultat.php");
 include_once("vues/v_notes.php");
+include_once("vues/v_pied.html");
 ?>
